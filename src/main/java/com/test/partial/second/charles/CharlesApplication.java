880a1ddf958bf6ff85c3272e4f6c4e96a21c5b36
Charles Zambrano <charleszambranoespe@outlook.com>
2024-07-31 18:28:33 -0500

    first commit

---
+++ src/main/java/com/test/partial/second/charles/CharlesApplication.java
@@ -0,0 +1,13 @@
+package com.test.partial.second.charles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/partial/second/charles/CharlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.partial.second.charles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
